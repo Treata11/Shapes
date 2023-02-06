6deaed7190a5846c099c1292f0049165948ec8b8
Treata11 <treata11@yahoo.com>
2023-02-06 19:41:48 +0330

    Create BadgeSymbol and a RotatedBadgeSymbol for representation

---
+++ Shapes/Landmarks/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Shapes
+//
+//  Created by Treata Norouzi on 2/6/23.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(0)
+            .rotationEffect(angle, anchor: .bottom)
+    
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(radians: 0.1))
+    }
+}
