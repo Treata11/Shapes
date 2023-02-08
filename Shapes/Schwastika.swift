a53ff5155a83d51d0e5657ef13f70af0d416ec95
Treata11 <treata11@yahoo.com>
2023-02-09 00:11:45 +0330

    Create Schwastika

---
+++ Shapes/Schwastika.swift
@@ -0,0 +1,105 @@
+//
+//  Schwastika.swift
+//  Shapes
+//
+//  Created by Treata Norouzi on 2/8/23.
+//
+
+import SwiftUI
+
+struct SchwastikaParameters {
+    struct Segment {
+            let line: CGPoint
+            let curve: CGPoint
+            let control: CGPoint
+        }
+    
+        static let segments = [
+            Segment(
+                line: CGPoint(x: 5, y: 0),
+                curve: CGPoint(x: 1.0, y: 1.0),
+                control: CGPoint(x: 1.0, y: 1.0)
+            ),
+            Segment(
+                line: CGPoint(x: 2, y: 0),
+                curve: CGPoint(x: 1.0, y: 1.0),
+                control: CGPoint(x: 1.0, y: 1.0)
+            ),
+            Segment(
+                line: CGPoint(x: 2, y: 4),
+                curve: CGPoint(x: 1.0, y: 1.0),
+                control: CGPoint(x: 1.0, y: 1.0)
+            ),
+            Segment(
+                line: CGPoint(x: 0, y: 4),
+                curve: CGPoint(x: 1.0, y: 1.0),
+                control: CGPoint(x: 1.0, y: 1.0)
+            ),
+            Segment(
+                line: CGPoint(x: 0, y: 5),
+                curve: CGPoint(x: 1.0, y: 1.0),
+                control: CGPoint(x: 1.0, y: 1.0)
+            ),
+            Segment(
+                line: CGPoint(x: 3, y: 5),
+                curve: CGPoint(x: 1.0, y: 1.0),
+                control: CGPoint(x: 1.0, y: 1.0)
+            ),
+            Segment(
+                line: CGPoint(x: 3, y: 1),
+                curve: CGPoint(x: 1.0, y: 1.0),
+                control: CGPoint(x: 1.0, y: 1.0)
+            ),
+            Segment(
+                line: CGPoint(x: 5, y: 1),
+                curve: CGPoint(x: 1.0, y: 1.0),
+                control: CGPoint(x: 1.0, y: 1.0)
+            )
+        ]
+}
+
+struct HalfSchwastika: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+
+                let width = ceil(min(geometry.size.width, geometry.size.height)) / 5.0
+                let height = width
+
+                let segmentStart = CGPoint(x: 5 * width, y: 0 * height)
+
+                path.move(to: CGPoint(
+                    x: segmentStart.x,
+                    y: segmentStart.y)
+                )
+                SchwastikaParameters.segments.forEach { segment in
+                    path.addLine(to: CGPoint(
+                        x: width * segment.line.x,
+                        y: height * segment.line.y)
+                    )
+                }
+            }
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+}
+
+struct Schwastika: View {
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                HalfSchwastika()
+                HalfSchwastika()
+                    .rotationEffect(Angle(degrees: 90))
+            }
+        }
+        .aspectRatio(1.0, contentMode: .fit)
+    }
+}
+
+struct 卐_Previews: PreviewProvider {
+    static var previews: some View {
+        Schwastika()
+            .foregroundColor(.orange)
+    }
+}
