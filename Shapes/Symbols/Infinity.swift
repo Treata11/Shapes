9c7080d0f4e3588de6d466ec70d4e8b72518544a
Treata11 <treata11@yahoo.com>
2023-02-09 18:58:12 +0330

    Create a ridiculous Infinity symbol, should be deleted and be created with addCurve in path

--- Shapes/Symbols/Infinity.swift
+++ Shapes/Symbols/Infinity.swift
@@ -7,7 +7,63 @@
 
 import SwiftUI
 
-struct Infinity: View {
+struct Infinity: InsettableShape {
+    
+    var insetAmount = 0.0
+    
+    func path(in rect: CGRect) -> Path {
+        
+        var p = Path()
+        
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.size.width, rect.size.height) / 4
+        let pointA = CGPoint(
+            x: center.x / 2.0,
+            y: center.y
+        )
+        let pointB = CGPoint(
+            x: center.x * 1.5,
+            y: center.y
+        )
+        
+        p.move(to: CGPoint(
+            x: pointA.x,
+            y: pointA.y)
+        )
+        p.addArc(
+            center: pointA,
+            radius: radius,
+            startAngle: Angle(radians: 0.0),
+            endAngle: Angle(radians: .pi * 2),
+            clockwise: false
+        )
+        
+        p.move(to: CGPoint(
+            x: pointB.x,
+            y: pointB.y)
+        )
+        p.addArc(
+            center: pointB,
+            radius: radius,
+            startAngle: Angle(radians: 0.0),
+            endAngle: Angle(radians: .pi * 2),
+            clockwise: false
+        )
+        
+        p.trim(from: pointA.x, to: center.x)
+        p.trim(from: pointB.x, to: rect.maxX)
+        
+        return p
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var line = self
+        line.insetAmount += amount
+        return line
+    }
+}
+
+struct PixelatedInfinity: View {
     var body: some View {
         GeometryReader { geometry in
             
@@ -26,13 +82,15 @@ struct Infinity: View {
             .trim(from: 0.25, to: 1.0)
             .scale(50, anchor: .topLeading)
             .stroke(Color.black, lineWidth: 3)
-            
         }
+        .aspectRatio(contentMode: .fit)
     }
 }
 
 struct Infinity_Previews: PreviewProvider {
     static var previews: some View {
         Infinity()
+            .stroke()
+            .aspectRatio(contentMode: .fit)
     }
 }
