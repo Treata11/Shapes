e25694edea4667af8b59c3d1f6d2a52dde3d02cd
Treata11 <treata11@yahoo.com>
2023-02-09 11:22:27 +0330

    Clean up some code

--- Shapes/Polygram/Octagram.swift
+++ Shapes/Polygram/Octagram.swift
@@ -11,17 +11,15 @@ struct Octagram: View {
     var body: some View {
         GeometryReader { geometry in
             let center = UnitPoint.center
-            let square = Rectangle()
-                .aspectRatio(1.0, contentMode: .fit)
-            let rotatedSquare = square
+            let rotatedSquare = RoundedRectangle(cornerSize: CGSize.zero)
                 .rotationEffect(Angle(degrees: 45), anchor: center)
             ZStack {
-                square
+                RoundedRectangle(cornerRadius: CGFloat(0))
                 rotatedSquare
             }
         }
         .aspectRatio(1.0, contentMode: .fit)
-        .scaleEffect(0.7)
+        .scaleEffect(0.70712)
     }
 }
 
