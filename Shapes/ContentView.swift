5895fff1b9732580296f53b3ae731bd4b413f27d
Treata11 <treata11@yahoo.com>
2023-02-09 16:53:20 +0330

    Sort out some code and some minor updates

--- Shapes/ContentView.swift
+++ Shapes/ContentView.swift
@@ -11,13 +11,28 @@ struct ContentView: View {
     @State private var insetAmount = 50.0
 
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 350, height: 200)
-            .onTapGesture {
-                withAnimation {
-                    insetAmount = Double.random(in: 10...90)
-                }
+        HStack {
+            GeometryReader { geometry in
+                
+                let dedicatedWidth = geometry.size.width / 5.0
+                let dedicatedHeight = geometry.size.height / 5.0
+                
+                Trapezoid(insetAmount: insetAmount)
+                    .frame(width: dedicatedWidth, height: dedicatedHeight)
+                    .position(x: 100, y: 100)
+                    .onTapGesture {
+                        withAnimation {
+                            insetAmount = Double.random(in: 20...90)
+                        }
+                    }
+                Pentagram()
+                    .frame(width: dedicatedWidth, height: dedicatedHeight)
+                    .position(x: 300, y: 250)
+                Hexagram()
+                    .frame(width: dedicatedWidth, height: dedicatedHeight)
             }
+        }
+        .aspectRatio(contentMode: .fit)
     }
 }
 
