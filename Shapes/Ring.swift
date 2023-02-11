fd97131ee16b76ff4dba9804f399920d75851566
Treata11 <treata11@yahoo.com>
2023-02-11 21:27:21 +0330

    Make thickness for _Ring.swift_ an optional

--- Shapes/Ring.swift
+++ Shapes/Ring.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct Ring: View {
+    var thickness: CGFloat?
+    
     var body: some View {
         GeometryReader { geometry in
             
             let width = geometry.size.width
-            
-            Circle()
-                .strokeBorder(lineWidth: width / 13)
+            if thickness != nil {
+                Circle()
+                    .strokeBorder(lineWidth: thickness! / 13)
+            } else {
+                Circle()
+                    .strokeBorder(lineWidth: width / 13)
+            }
         }
         .aspectRatio(1.0, contentMode: .fit)
     }
