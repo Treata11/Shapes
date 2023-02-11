7955d85724264fcfd8a7dfed78ace71f18537b62
Treata11 <treata11@yahoo.com>
2023-02-11 16:51:30 +0330

    Successfully and Beautifully Created SettingsGears, Next I would create a ProgressiveView() of it to replace the original one

--- Shapes/Complex Views/SettingsGear.swift
+++ Shapes/Complex Views/SettingsGear.swift
@@ -63,50 +63,45 @@ struct SettingsGear: View {
 }
 
 struct WheelDriveGear: View {
-    
-    struct WheelDriveParameters {
-        struct Segment {
-            let line: CGPoint
-            let curve: CGPoint
-            let control: CGPoint
-        }
-
-//        static var segments = [
-//            Segment(
-//                line: CGPoint(x: 0.5, y: 0.5),
-//                curve: ,
-//                control:
-//            ),
-//        ]
-    }
-    
     var body: some View {
         GeometryReader { geometry in
             let width = CGPoint(
                 x: geometry.size.width / 2.0,
                 y: geometry.size.height / 2.0
             )
-            let height = width
-            
-            SettingsGear()
-            
+            ZStack {
+                WheelDrivePillars()
+                    .scaleEffect(0.9)
+                SettingsGear()
+            }
         }
         .aspectRatio(1.0, contentMode: .fit)
     }
 }
 
-//struct SettingsGear_Previews: PreviewProvider {
-//    static var previews: some View {
-//        SettingsGear()
-//            .aspectRatio(1.0, contentMode: .fit)
-//            .foregroundColor(.gray)
-//    }
-//}
+struct SettingsGears: View {
+    var body: some View {
+        ZStack {
+            WheelDriveGear()
+            SettingsGear()
+                .scaleEffect(0.55)
 
-struct WheelDriveGear_Previews: PreviewProvider {
-    static var previews: some View {
-        WheelDriveGear()
-            .foregroundColor(.gray)
-            .aspectRatio(contentMode: .fit)
+        }
     }
 }
+
+struct SettingsGear_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsGears()
+            .aspectRatio(1.0, contentMode: .fit)
+            .foregroundColor(.gray)
+    }
+}
+
+//struct WheelDriveGear_Previews: PreviewProvider {
+//    static var previews: some View {
+//        WheelDriveGear()
+//            .foregroundColor(.gray)
+//            .aspectRatio(contentMode: .fit)
+//    }
+//}
