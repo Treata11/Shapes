62213b4dfe5a3623f6d03ffb927f0613ae05862f
Treata11 <treata11@yahoo.com>
2023-02-13 15:52:09 +0330

    Successfully Create *FetchingView()*

---
+++ Shapes/Complex Views/Safari/FetchingView.swift
@@ -0,0 +1,41 @@
+//
+//  FetchingView.swift
+//  Shapes
+//
+//  Created by Treata Norouzi on 2/13/23.
+//
+
+import SwiftUI
+
+struct RotatingCompassNeedle: View {
+    let angle: Angle
+    
+    var body: some View {
+        ZStack {
+            CompassNeedle()
+        }
+        .rotationEffect(angle)
+    }
+}
+
+struct FetchingView: View {
+    @State private var rotationAngle: Angle = .degrees(50)
+    
+    var body: some View {
+        ZStack {
+            Dials()
+            RotatingCompassNeedle(angle: rotationAngle)
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 3).repeatForever()) {
+                        rotationAngle -= .degrees(100)
+                    }
+                }
+        }
+    }
+}
+
+struct FetchingView_Previews: PreviewProvider {
+    static var previews: some View {
+        FetchingView()
+    }
+}
