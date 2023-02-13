ce33220c254d38fc3abfd813c53b1f34eb00448b
Treata11 <treata11@yahoo.com>
2023-02-13 21:39:25 +0330

    Create Photos icon *Initial approach with .fram*

---
+++ Shapes/Complex Views/Photos.swift
@@ -0,0 +1,81 @@
+//
+//  Photos.swift
+//  Shapes
+//
+//  Created by Treata Norouzi on 2/13/23.
+//
+
+import SwiftUI
+
+struct Photos: View {
+    
+    var body: some View {
+        ZStack {
+            Circle()    // rotate copies along this path
+                .frame(width: 75, height: 75)
+                .opacity(0)
+            
+             VividCapsule()
+                .foregroundColor(.blue)
+                .rotationEffect(.degrees(0))
+            VividCapsule()
+               .foregroundColor(.purple)
+               .rotationEffect(.A45)
+            VividCapsule()
+                .foregroundColor(.pink)
+               .rotationEffect(.A90)
+               .contrast(0.1)
+               .saturation(9)
+            VividCapsule()
+                .foregroundColor(.red)
+                .rotationEffect(.A90 + .A45)
+            VividCapsule()
+                .foregroundColor(.orange)
+                .rotationEffect(.A180)
+            VividCapsule()
+                .foregroundColor(.yellow)
+                .rotationEffect(.A180 + .A45)
+            VividCapsule()
+                .foregroundColor(.green)
+                .rotationEffect(.A270)
+            VividCapsule()
+                .foregroundColor(.green)
+                .rotationEffect(-.A45)
+                .contrast(0.1)
+                .saturation(7)
+        }
+        .frame(width: 500, height: 500)
+        .scaleEffect(2)
+        .aspectRatio(contentMode: .fit)
+    }
+}
+
+struct VividCapsule: View {
+    var body: some View {
+        Capsule(style: .continuous)
+            .offset(y: 38)
+            .frame(width: 50, height: 70)
+            .opacity(0.75)
+    }
+}
+
+
+//struct VerticalCapsule: View {
+//    let rotationAngle: Angle
+//    let anchorPoint: UnitPoint
+//
+//    var body: some View {
+//        GeometryReader { geo in
+//            Capsule(style: .continuous)
+//        }
+//        .rotationEffect(rotationAngle, anchor: anchorPoint)
+//        .aspectRatio(0.65823, contentMode: .fit)
+//        .opacity(0.7)
+//    }
+//}
+
+struct Photos_Previews: PreviewProvider {
+    static var previews: some View {
+        Photos()
+    }
+}
--- Shapes/UtilityExtensions.swift
+++ Shapes/UtilityExtensions.swift
@@ -30,6 +30,7 @@ extension CGRect {
 }
 
 extension Angle {
+    static let A0 = A90 * 0
     static let A45 = A90 / 2
     static let A90 = Angle(radians: .pi/2)  // 90 degrees
     static let A180 = A90 * 2  // 180 degrees
