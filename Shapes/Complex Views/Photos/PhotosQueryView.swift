31473d4b4a0f05461b348577d55b0c5e4cca0349
Treata11 <treata11@yahoo.com>
2023-02-15 21:34:09 +0330

    Create PhotosQueryView()

---
+++ Shapes/Complex Views/Photos/PhotosQueryView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotosQueryView.swift
+//  Shapes
+//
+//  Created by Treata Norouzi on 2/15/23.
+//
+
+import SwiftUI
+
+struct PhotosQueryView: View {
+    var body: some View {
+        
+    }
+}
+
+struct PhotosQueryView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotosQueryView()
+    }
+}
