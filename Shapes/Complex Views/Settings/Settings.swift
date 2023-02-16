9b018e37b152c3489c0f9f97217e0c8709707ecf
Treata11 <treata11@yahoo.com>
2023-02-16 17:18:03 +0330

    Typoo

---
+++ Shapes/Complex Views/Settings/Settings.swift
@@ -0,0 +1,20 @@
+//
+//  Settings.swift
+//  Shapes
+//
+//  Created by Treata Norouzi on 2/16/23.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
