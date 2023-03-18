a42312db26070e7e1990f1cd53ba6d4e83e07740
Treata11 <treata11@yahoo.com>
2023-03-18 17:27:34 +0330

    got the rotation animation for RotatedCapsules working, but there's an issue with the withAnimation() (i.e. SwiftUI's precanned explicite animation system) that isn't the way I need it to be (withAnimation doesn't take the delays into account and animate the final value of the @State vars

--- Shapes/Complex Views/Photos/Enhanced /_EnhancedPhotos.swift
+++ Shapes/Complex Views/Photos/Enhanced /_EnhancedPhotos.swift
@@ -23,38 +23,86 @@ struct Photo: View {
     var body: some View {
         ZStack {
             ForEach(0..<8) { index in
-                var precededIndices = index
                 RotatedCapsule(id: index, color: ColorConstants.colors[index], rotation: angle[index])
                     .onAppear() {
+                        var precededIndices = index
                         while precededIndices >= 0 {
-                            withAnimation(dealAnimation(for: RotatedCapsule(id: index, color: ColorConstants.colors[index], rotation: angle[index]), index: index)) {
+                            withAnimation(dealAnimation(
+                                for: RotatedCapsule(
+                                    id: precededIndices,
+                                    color: ColorConstants.colors[precededIndices],
+                                    rotation: angle[precededIndices]
+                                ),
+                                index: precededIndices)) {
                                 angle[precededIndices] += .a45
                                 precededIndices -= 1
                             }
                         }
                     }
             }
-//            RotatedCapsule(id: 0, color: .orange, rotation: angle[0])
+            
+            
+            
+//            RotatedCapsule(id: 0, color: ColorConstants.colors[0], rotation: angle0)
 //                .onAppear() {
-//                    withAnimation(dealAnimation(for: self, index: 0)) {
-//                        angle[0] += .a45
+//                    withAnimation(.easeInOut(duration: 0.5).delay(0)) {
+//                        angle0 += .a45
 //                }
 //            }
-//            RotatedCapsule(id: 1, color: .red, rotation: angle[1])
+//            RotatedCapsule(id: 1, color: ColorConstants.colors[1], rotation: angle1)
 //                .onAppear() {
-//                    withAnimation(dealAnimation(for: self, index: 1)) {
-//                        angle[1] += .a45
+//                    withAnimation(.easeInOut(duration: 0.5).delay(0.5)) {
+//                        angle0 += .a45
+//                        angle1 += .a45
 //                }
 //            }
+//            RotatedCapsule(id: 2, color: ColorConstants.colors[2], rotation: angle2)
+//                .onAppear() {
+//                    withAnimation(.easeInOut(duration: 0.5).delay(1)) {
+//                        angle0 += .a45
+//                        angle1 += .a45
+//                        angle2 += .a45
+//                }
+//            }
+//            RotatedCapsule(id: 3, color: ColorConstants.colors[3], rotation: angle3)
+//                .onAppear() {
+//                    withAnimation(.easeInOut(duration: 0.5).delay(1.5)) {
+//                        angle0 += .a45
+//                        angle1 += .a45
+//                        angle2 += .a45
+//                        angle3 += .a45
+//                }
+//            }
+//            RotatedCapsule(id: 4, color: ColorConstants.colors[4], rotation: angle4)
+//                .onAppear() {
+//                    withAnimation(.easeInOut(duration: 0.5).delay(2)) {
+//                        angle0 += .a45
+//                        angle1 += .a45
+//                        angle2 += .a45
+//                        angle3 += .a45
+//                        angle4 += .a45
+//                }
+//            }
+//            RotatedCapsule(id: 5, color: ColorConstants.colors[5], rotation: angle5)
+//                .onAppear() {
+//                    withAnimation(.easeInOut(duration: 0.5).delay(2.5)) {
+//                        angle0 += .a45
+//                        angle1 += .a45
+//                        angle2 += .a45
+//                        angle3 += .a45
+//                        angle4 += .a45
+//                        angle5 += .a45
+//                }
+//            }
+            
         }
         .scaledToFit()
     }
     
     func dealAnimation(for shape: any View, index: Int) -> Animation {
         var delay = 0.0
-        delay = Double(index * (AnimationConstats.totalDealDuration / 8))
-        return Animation.easeInOut(duration: Double(AnimationConstats.dealDuration))
-            .repeatForever(autoreverses: false)
+        delay = Double(index * (AnimationConstants.totalDealDuration / 8))
+        return Animation.easeInOut(duration: Double(AnimationConstants.dealDuration))
             .delay(delay)
     }
 }
@@ -84,7 +132,7 @@ private struct ColorConstants {
     ]
 }
 
-private struct AnimationConstats {
+private struct AnimationConstants {
     static let dealDuration = 1
     static let totalDealDuration = 8
 }
